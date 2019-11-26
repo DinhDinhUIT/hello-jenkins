e4af6b6e4ab65b563afd32cdfc99a53d3ee7dc46
DinhDinhUIT <55026731+DinhDinhUIT@users.noreply.github.com>
2019-11-26 15:44:34 +0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  xit('respond with hello world', function(done) {
+  it('respond with hello world', function(done) {
     request(app).get('/').expect('hello world', done);
   });
 });
