a5bdce09ada2cb17a6bf1f8a79564f84c7e69dc3
Dinh Dinh <dinhtd@gmail.com>
2019-11-26 02:09:47 -0800

    add mocha exit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
  
 var app = express();
+
  
 app.get('/', function (req, res) {
   res.send('hello world');
 });
  
-app.listen(process.env.PORT || 8000);
+var server=app.listen(process.env.PORT || 3000);
  
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@
 var request = require('supertest');
 var app = require('../app.js');
  
-describe.only('GET /', function() {
+describe('GET /', function() {
   it('respond with hello world', function(done) {
     request(app).get('/').expect('hello world', done);
   });
 });
+
