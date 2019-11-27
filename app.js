bbde7c0fb30adbcf15c7b2acc27d5e09aa833f1e
Dinh Dinh <dinhtd@gmail.com>
2019-11-27 02:35:04 -0800

    update code

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express');
  
 var app = express();
 
- 
-app.get('/', function (req, res) {
-  res.send('hello world');
+app.get('/dinh', function (req, res) {
+  res.send('hello Dinh Dinh');
 });
- 
+
+app.get('/tuan', function (req, res) {
+  res.send('hello Hoang Tuan');
+});
+
+app.get('/thang', function (req, res) {
+  res.send('hello Long Thang');
+});
+
+
 var server=app.listen(process.env.PORT || 3000);
  
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,9 +2,20 @@
 var request = require('supertest');
 var app = require('../app.js');
  
-describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+describe('GET /dinh', function() {
+  it('respond with Dinh Dinh', function(done) {
+    request(app).get('/dinh').expect('hello Dinh Dinh', done);
   });
 });
 
+describe('GET /tuan', function() {
+  it('respond with Hoang Tuan', function(done) {
+    request(app).get('/tuan').expect('hello Hoang Tuan', done);
+  });
+});
+
+describe('GET /thang', function() {
+  it('respond with Long Thang', function(done) {
+    request(app).get('/thang').expect('hello Long Thang', done);
+  });
+});
